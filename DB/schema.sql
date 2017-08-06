44c776c801cdfe770a359b90fcbd0b0ca5ba7f44
小笠原 奈々 <shiningcureseven@gmail.com>
2017-08-06 21:37:17 +0900

    docker comporse

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,16 +1,19 @@
+CREATE DATABASE product_admin;
+USE product_admin;
+
 CREATE TABLE product (
-  id UNSIGNED BIGINT NOT NULL AUTO_INCREMENT,
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   title VARCHAR(100) NOT NULL,
   description VARCHAR(500),
   price DECIMAL(11, 2) NOT NULL,
   PRIMARY KEY (id),
   KEY key_price (price)
-)
+) DEFAULT CHARSET=utf8mb4 ENGINE=InnoDB;
 
 CREATE TABLE product_picture (
-  id UNSIGNED BIGINT NOT NULL AUTO_INCREMENT,
+  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   product_id BIGINT NOT NULL,
   sha_256 BINARY(32) NOT NULL,
   PRIMARY KEY (id),
   KEY key_product_id (product_id)
-)
+) DEFAULT CHARSET=utf8mb4 ENGINE=InnoDB;
