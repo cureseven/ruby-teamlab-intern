6ebc5621bb98a61a5e171210c035e2e243e05343
小笠原 奈々 <shiningcureseven@gmail.com>
2017-09-12 21:45:43 +0900

    docker-composeにwebの定義を追加

--- ruby-teamlab/db/migrate/20170908045833_create_products.rb
+++ ruby-teamlab/db/migrate/20170908045833_create_products.rb
@@ -1,9 +1,9 @@
-class CreateProducts < ActiveRecord::Migration[5.1]
+ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-#      t.string :title
-#      t.string :discription
-#      t.integer :price
+      t.string :title
+      t.string :discription
+      t.integer :price
 
       t.timestamps
     end
